195555250ef2dccaaf7a087f2bdc17cc6adb629d
Suyeol Yun <suyeolyun@icloud.com>
2024-10-03 15:02:10 -0400

    add

--- train-rl-agents.py
+++ train-rl-agents.py
@@ -45,27 +45,112 @@ e_train_gym = StockTradingEnv(df = train, **env_kwargs)
 env_train, _ = e_train_gym.get_sb_env()
 print(type(env_train))
 
-agent = DRLAgent(env = env_train)
-
 # Set the corresponding values to 'True' for the algorithms that you want to use
-if_using_a2c = True
-# if_using_ddpg = True
-# if_using_ppo = True
-# if_using_td3 = True
-# if_using_sac = True
-
-agent = DRLAgent(env = env_train, n_envs=10)
-model_a2c = agent.get_model("a2c")
+# if_using_a2c = True
+if_using_a2c = False # already trained
+if_using_ddpg = True
+if_using_ppo = True
+if_using_td3 = True
+if_using_sac = True
 
 if if_using_a2c:
-  # set up logger
-  tmp_path = RESULTS_DIR + '/a2c'
-  new_logger_a2c = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-  # Set new logger
-  model_a2c.set_logger(new_logger_a2c)
+    print("training A2C agent")
+    agent = DRLAgent(env = env_train)
+    model_a2c = agent.get_model("a2c")
 
-trained_a2c = agent.train_model(model=model_a2c, 
-                             tb_log_name='a2c',
-                             total_timesteps=50000) if if_using_a2c else None
+    # set up logger
+    tmp_path = RESULTS_DIR + '/a2c'
+    new_logger_a2c = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model_a2c.set_logger(new_logger_a2c)
 
-trained_a2c.save(TRAINED_MODEL_DIR + "/agent_a2c") if if_using_a2c else None
+    trained_a2c = agent.train_model(model=model_a2c, 
+                                    tb_log_name='a2c',
+                                    total_timesteps=50000) if if_using_a2c else None
+
+    trained_a2c.save(TRAINED_MODEL_DIR + "/agent_a2c") if if_using_a2c else None
+
+if if_using_ddpg:
+    print("training DDPG agent")
+    agent = DRLAgent(env = env_train)
+    model_ddpg = agent.get_model("ddpg")
+
+    # set up logger
+    tmp_path = RESULTS_DIR + '/ddpg'
+    new_logger_ddpg = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model_ddpg.set_logger(new_logger_ddpg)
+
+    trained_ddpg = agent.train_model(model=model_ddpg, 
+                             tb_log_name='ddpg',
+                             total_timesteps=50000) if if_using_ddpg else None
+    
+    trained_ddpg.save(TRAINED_MODEL_DIR + "/agent_ddpg") if if_using_ddpg else None
+
+if if_using_ppo:
+    print("training PPO agent")
+    agent = DRLAgent(env = env_train)
+    PPO_PARAMS = {
+        "n_steps": 2048,
+        "ent_coef": 0.01,
+        "learning_rate": 0.00025,
+        "batch_size": 128,
+    }
+    model_ppo = agent.get_model("ppo",model_kwargs = PPO_PARAMS)
+
+    # set up logger
+    tmp_path = RESULTS_DIR + '/ppo'
+    new_logger_ppo = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model_ppo.set_logger(new_logger_ppo)
+
+    trained_ppo = agent.train_model(model=model_ppo, 
+                             tb_log_name='ppo',
+                             total_timesteps=200000) if if_using_ppo else None
+    
+    trained_ppo.save(TRAINED_MODEL_DIR + "/agent_ppo") if if_using_ppo else None
+
+if if_using_td3:
+    print("training TD3 agent")
+    agent = DRLAgent(env = env_train)
+    TD3_PARAMS = {"batch_size": 100, 
+                "buffer_size": 1000000, 
+                "learning_rate": 0.001}
+
+    model_td3 = agent.get_model("td3",model_kwargs = TD3_PARAMS)
+
+    # set up logger
+    tmp_path = RESULTS_DIR + '/td3'
+    new_logger_td3 = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model_td3.set_logger(new_logger_td3)
+
+    trained_td3 = agent.train_model(model=model_td3, 
+                             tb_log_name='td3',
+                             total_timesteps=50000) if if_using_td3 else None
+    
+    trained_td3.save(TRAINED_MODEL_DIR + "/agent_td3") if if_using_td3 else None
+
+if if_using_sac:
+    print("training SAC agent")
+    agent = DRLAgent(env = env_train)
+    SAC_PARAMS = {
+        "batch_size": 128,
+        "buffer_size": 100000,
+        "learning_rate": 0.0001,
+        "learning_starts": 100,
+        "ent_coef": "auto_0.1",
+    }
+
+    model_sac = agent.get_model("sac",model_kwargs = SAC_PARAMS)
+
+    # set up logger
+    tmp_path = RESULTS_DIR + '/sac'
+    new_logger_sac = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model_sac.set_logger(new_logger_sac)
+
+    trained_sac = agent.train_model(model=model_sac, 
+                             tb_log_name='sac',
+                             total_timesteps=70000) if if_using_sac else None
+    trained_sac.save(TRAINED_MODEL_DIR + "/agent_sac") if if_using_sac else None
