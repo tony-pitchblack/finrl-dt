f40808aee8367bf599621c2f15a6cc790ccc32d2
Suyeol Yun <suyeolyun@icloud.com>
2024-10-04 13:41:37 -0400

    add ddpg

--- collect_samples_parallel.py
+++ collect_samples_parallel.py
@@ -15,15 +15,14 @@ from datetime import datetime
 from finrl.config import INDICATORS, TRAINED_MODEL_DIR
 
 def init_worker(model, train_data):
-    # Set global variables for the worker processes
-    global model_a2c_global
+    global model_global
     global train_data_global
-    model_a2c_global = model
+    model_global = model
     train_data_global = train_data
 
 def collect_single_trajectory(env_kwargs):
     # Use the global model and data
-    model_a2c = model_a2c_global
+    model = model_global
     train_data = train_data_global
 
     # Initialize the environment within the process
@@ -45,8 +44,7 @@ def collect_single_trajectory(env_kwargs):
 
     while not done:
         # Use the trained A2C model to select actions
-        # action, _ = model_a2c.predict(state, deterministic=True)
-        action, _ = model_a2c.predict(state, deterministic=False)
+        action, _ = model.predict(state, deterministic=False)
         
         next_state, reward, done, truncated, info = env.step(action)
 
@@ -74,9 +72,15 @@ if __name__ == "__main__":
     train.index.names = ['']
 
     # Load the model once
-    from stable_baselines3 import A2C
-    model_path = TRAINED_MODEL_DIR + "/agent_a2c"
-    model_a2c = A2C.load(model_path)
+    from stable_baselines3 import A2C, DDPG
+    while model_choice not in ['a2c', 'ddpg']:
+        model_choice = input("Invalid choice. Please enter 'a2c' or 'ddpg': ").lower()
+    
+    model_path = TRAINED_MODEL_DIR + f"/agent_{model_choice}"
+    if model_choice == 'a2c':
+        model = A2C.load(model_path)
+    elif model_choice == 'ddpg':
+        model = DDPG.load(model_path)
 
     # Define environment parameters
     stock_dimension = len(train.tic.unique())
@@ -112,7 +116,7 @@ if __name__ == "__main__":
     env_args_list = [env_kwargs for _ in range(total_episodes)]
 
     # Use a persistent Pool and initialize with shared resources
-    with multiprocessing.Pool(processes=max_workers, initializer=init_worker, initargs=(model_a2c, train)) as pool:
+    with multiprocessing.Pool(processes=max_workers, initializer=init_worker, initargs=(model, train)) as pool:
         trajectories = list(tqdm(
             pool.imap_unordered(collect_single_trajectory, env_args_list),
             total=total_episodes,
@@ -121,8 +125,8 @@ if __name__ == "__main__":
 
     # Save the trajectories to a pickle file
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    output_filename = f'trajectories_a2c_{total_episodes}_{current_time}.pkl'
+    output_filename = f'trajectories_{model_choice}_{total_episodes}_{current_time}.pkl'
     with open(output_filename, 'wb') as f:
         pickle.dump(trajectories, f)
 
-    print(f"Collected {total_episodes} trajectories and saved to '{output_filename}'")
+    print(f"Collected {total_episodes} trajectories using {model_choice.upper()} model and saved to '{output_filename}'")
