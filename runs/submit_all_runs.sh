c374dd5188b8b7961344ae0fc32c0da75192b600
Suyeol Yun <suyeolyun@icloud.com>
2024-10-12 14:16:05 -0400

    add

--- runs/submit_all_runs.sh
+++ runs/submit_all_runs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Submit all run scripts
-sbatch submit_run_a2c_dt_gpu.sh
-sbatch submit_run_ddpg_dt_gpu.sh
-sbatch submit_run_td3_dt_gpu.sh
-sbatch submit_run_ppo_dt_gpu.sh
-sbatch submit_run_sac_dt_gpu.sh
-sbatch submit_run_ensemble_dt_gpu.sh
+sbatch runs/run_a2c_dt_gpu.sh
+sbatch runs/run_ddpg_dt_gpu.sh
+sbatch runs/run_td3_dt_gpu.sh
+sbatch runs/run_ppo_dt_gpu.sh
+sbatch runs/run_sac_dt_gpu.sh
+sbatch runs/run_ensemble_dt_gpu.sh
